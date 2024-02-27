eadbbfc5687a776679baa957a6e999f5c306ce95
Laibakhan20 <161496627+Laibakhan20@users.noreply.github.com>
2024-02-27 22:30:33 +0500

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,32 @@
+"use strict";
+// ASSIGNMENT OF CLASS
+// TASK 1:
+// CREATING EXAMPLES OF DATATYPES:
+// 1. EXAMPLE OF "STRING" DATATYPE WITH CONCATENATION:
+let country = "Pakistan";
+let city = "karachi";
+console.log("I love " + country, " & i live in " + city);
+// 2. EXAMPLE OF "NUMBER" WITH ARITHMETIC OPERATION:
+let a = 5;
+let b = 5;
+console.log("a = ", a, " & b = ", b);
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a * b = ", a * b);
+console.log("a / b = ", a / b);
+console.log("a % b = ", a % b);
+// 3.EXAMPLE OF "BOOLEAN" DATAYPE:
+let isPresent = true;
+console.log(isPresent);
+// 4. EXAMPLE OF "NULL" DATATYPE:
+let x = null;
+console.log(x);
+// 5. EXAMMPLE OF "UNDEFINED" DATATYPE:
+let y = undefined;
+console.log(y);
+// 6. EXAMPLE OF "BIGINT" DTATYPE:
+let z = BigInt("123456789");
+console.log(z);
+// 7. EXAMPLE OF "SYMBOL" DATATYPE:
+let c = Symbol("Hey ! ");
+console.log(c);
